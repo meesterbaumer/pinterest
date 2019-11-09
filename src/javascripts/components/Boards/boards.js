74bcb6fe2ef4d7f32b0e161b145259741fe4250a
Michael Baumer <meesterbaumer@mac.com>
2019-11-09 11:01:42 -0600

    WIP

---
+++ src/javascripts/components/Boards/boards.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import apiKeys from '../../helpers/data/apiKeys.json';
+
+
+const buildBoard = () => {
+  axios.get('${baseUrl}/')
+  let domString = '';
+
+};
+
+export default { buildBoard };
