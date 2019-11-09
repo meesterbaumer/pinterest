f44cf946f50cf765a0d59bfc84191a4acf5ec8e0
Michael Baumer <meesterbaumer@mac.com>
2019-11-09 13:54:19 -0600

    boards printing

--- src/javascripts/components/Boards/boards.js
+++ src/javascripts/components/Boards/boards.js
@@ -1,11 +1,27 @@
-import axios from "axios";
-import apiKeys from '../../helpers/data/apiKeys.json';
+import utilities from '../../helpers/utilities';
+import boardsData from '../../helpers/data/boardsData';
+import 
 
-
-const buildBoard = () => {
-  axios.get('${baseUrl}/')
-  let domString = '';
-
+const printBoards = (uid) => {
+  boardsData.getBoardByUid(uid)
+    .then((boards) => {
+      let domString = '<h2>Pinterest</h2>';
+      domString += '<div id="board-section" class="d-flex flex-wrap">';
+      boards.forEach((board) => {
+        domString += `
+        <div class="card ${board.id} main-board" style="width: 18rem;">
+        <div class="card-body ${board.uid}">
+        <h5 class="card-title">${board.name}</h5>
+        <p class="card-text">${board.description}</p>
+        <a id="pin-button" href="#" class="btn btn-primary">View Pins</a>
+        </div>
+        </div>
+        `;
+      });
+      domString += '</div>';
+      utilities.printToDom('board-section', domString);
+    })
+    .catch((error) => console.error(error));
 };
 
-export default { buildBoard };
+export default { printBoards };
